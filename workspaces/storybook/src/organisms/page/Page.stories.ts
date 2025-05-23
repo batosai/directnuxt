c65a5791a18f1f9a3cb267d94633719028e27e61
jeremy <jeremy.chaufourier@opsone.net>
2025-05-24 00:21:23 +0200

    Add storybook

---
+++ workspaces/storybook/src/organisms/page/Page.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import MyPage from './Page.vue';
+
+const meta = {
+  title: 'Organisms/Page',
+  component: MyPage,
+  render: () => ({
+    components: { MyPage },
+    template: '<my-page />',
+  }),
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof MyPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on component testing: https://storybook.js.org/docs/writing-tests/component-testing
+export const LoggedIn: Story = {
+  play: async () => {
+    
+  },
+};
+
+export const LoggedOut: Story = {};
