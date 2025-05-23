e041536f64e8090a1f14f2aaa7a3f87359200cea
jeremy <jeremy.chaufourier@opsone.net>
2025-05-24 00:31:08 +0200

    Remove storybook in Nuxt

--- workspaces/nuxt-app/nuxt.config.ts
+++ workspaces/nuxt-app/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'nuxt-directus', '@nuxtjs/storybook'],
+  modules: ['@nuxt/eslint', 'nuxt-directus'],
   runtimeConfig: {
     public: {
       directusUrl: process.env.DIRECTUS_URL || 'http://localhost:8055'
