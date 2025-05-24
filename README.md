ddd4b5a7348e8fc628f19e1e8409803592fbff8b
jeremy <jeremy.chaufourier@opsone.net>
2025-05-25 00:21:18 +0200

    chore: add extensions SEO

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ Nuxt: http://localhost:3000
 Directus: http://localhost:8055
 Storybook: http://localhost:6006
 Vitepress: http://localhost:5173
+
+## DOC
+
+https://nuxt.com/docs
+https://nuxtseo.com
+https://directus.io/docs/
+https://storybook.js.org/docs
+https://vitepress.dev/
--- workspaces/nuxt-app/nuxt.config.ts
+++ workspaces/nuxt-app/nuxt.config.ts
@@ -2,12 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+  modules: ['@nuxt/eslint', '@nuxtjs/sitemap', '@nuxtjs/robots', 'nuxt-schema-org', 'nuxt-og-image'],
   routeRules: {
     // Generated at build time for SEO purpose
     '/': { prerender: true },
     // Cached for 1 hour
     '/api/*': { 
+      cache: process.env.NODE_ENV === 'production' 
+        ? { maxAge: process.env.CACHE_MAX_AGE ? parseInt(process.env.CACHE_MAX_AGE) : 60 * 60 }
+        : false
+     },
+     '/assets/*': { 
       cache: process.env.NODE_ENV === 'production' 
         ? { maxAge: process.env.CACHE_MAX_AGE ? parseInt(process.env.CACHE_MAX_AGE) : 60 * 60 }
         : false
